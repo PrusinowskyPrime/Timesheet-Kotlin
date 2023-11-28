6171171201e2eb6999485997c5a499c6060c27fa
Bartosz Michalski <bartosz.michalski777@gmail.com>
2023-11-28 21:36:08 +0100

    fix(work): switch to single-expression functions

--- src/main/kotlin/pl/prusinowsky/timesheet/work/service/WorkService.kt
+++ src/main/kotlin/pl/prusinowsky/timesheet/work/service/WorkService.kt
@@ -11,13 +11,9 @@ import pl.prusinowsky.timesheet.work.repository.WorkRepository
 class WorkService @Autowired constructor(
     private val workRepository: WorkRepository
 ) {
-    fun getAll(): List<WorkEntity> {
-        return workRepository.findAll()
-    }
+    fun getAll(): List<WorkEntity> = workRepository.findAll()
 
-    fun getById(id: String): WorkEntity? {
-        return workRepository.findById(id).orElse(null)
-    }
+    fun getById(id: String): WorkEntity? = workRepository.findById(id).orElse(null)
 
     fun create(data: CreateWork): WorkEntity {
         return workRepository.save(
@@ -33,24 +29,22 @@ class WorkService @Autowired constructor(
     fun update(id: String, data: UpdateWork): WorkEntity? {
         val existingWork = workRepository.findById(id)
 
-        if (existingWork.isPresent) {
-            val work = existingWork.get()
-
-            return workRepository.save(
-                WorkEntity(
-                    id = work.id,
-                    name = data.name,
-                    description = data.description,
-                    started = data.started,
-                    finished = data.finished
-                )
-            )
+        if (!existingWork.isPresent) {
+            return null
         }
 
-        return null
+        val work = existingWork.get()
+
+        return workRepository.save(
+            WorkEntity(
+                id = work.id,
+                name = data.name,
+                description = data.description,
+                started = data.started,
+                finished = data.finished
+            )
+        )
     }
 
-    fun delete(id: String) {
-        workRepository.deleteById(id)
-    }
+    fun delete(id: String): Unit = workRepository.deleteById(id)
 }
